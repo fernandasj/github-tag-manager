8cff296079c026cd41da35b63ade6bac253bc4b0
fernandasj <fernanda.silva_sj@hotmail.com>
2020-06-26 14:16:12 -0300

    :white_check_mark: Adicionado teste para o cadastro de um novo repositório

--- github/tests.py
+++ github/tests.py
@@ -1,3 +1,20 @@
+from django.urls import reverse
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient 
+
+from github import models as exams
+
+class RepositoryTest(TestCase):
+
+    def test_create_test(self):
+
+        user = User.objects.create_user('ada', 'ada@lovelace.com', 'adapassword')
+        url = reverse('github-api:api-github-list')
+
+        data = {"name": "django-girls-tutorial", "description":"Implementando um blog com Django", "user": user.pk}
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED) 
